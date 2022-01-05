7ca69f8108ee3918ab46ec1064ca35e499e5f75c
timmorrisdev <timmorris_16@hotmail.com>
2022-01-05 18:21:18 +0000

    Add products app and add categroy and product models to database

---
+++ products/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import Product, Category
+
+
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'code',
+#         'name',
+#         'category',
+#         'price',
+#         'rating',
+#         'image',
+#     )
+
+#     ordering = ('code',)
+
+# class CategoryAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'friendly_name',
+#         'name',
+#     )
+
+admin.site.register(Product)
+admin.site.register(Category)
---
+++ products/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+
+class Category(models.Model):
+    ''' Model for product categories '''
+
+    class Meta:
+        ''' Model to override default addition of s for plural '''
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        ''' Override default object name return '''
+        return self.name
+
+    def get_friendly_name(self):
+        ''' Return friendly name of category '''
+        return self.friendly_name
+
+
+class Product(models.Model):
+    ''' Model for store products '''
+    
+    code = models.CharField(max_length=50,
+                            null=True,
+                            blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    brand = models.CharField(max_length=254)
+    category = models.ForeignKey('Category',
+                                 null=True,
+                                 blank=True,
+                                 on_delete=models.SET_NULL)
+    rating = models.DecimalField(max_digits=6, decimal_places=2,
+                                 null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
