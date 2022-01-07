f1419eaba5ed5bd11842713c1feff50b19458514
timmorrisdev <timmorris_16@hotmail.com>
2022-01-07 15:35:18 +0000

    add bag app, templates and views

--- bag/contexts.py
+++ bag/contexts.py
@@ -5,7 +5,7 @@ from products.models import Product
 
 
 def bag_contents(request):
-
+    ''' function to return contents of the bag to the site '''
     bag_items = []
     total = 0
     product_count = 0
@@ -34,7 +34,7 @@ def bag_contents(request):
                 })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
--- bag/views.py
+++ bag/views.py
@@ -19,6 +19,7 @@ def add_to_bag(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
+
     if 'product_size' in request.POST:
         size = request.POST['product_size']
     bag = request.session.get('bag', {})
