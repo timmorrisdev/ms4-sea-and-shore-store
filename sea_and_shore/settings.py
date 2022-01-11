5fa14d6c36d12aa81c100bfd18fb56a13f108474
timmorrisdev <timmorris_16@hotmail.com>
2022-01-11 11:50:55 +0000

    Set up confirmation email and templates

--- sea_and_shore/settings.py
+++ sea_and_shore/settings.py
@@ -180,6 +180,7 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET_SEASHORE', '')
+DEFAULT_FROM_EMAIL = 'seaandshore@example.com'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
