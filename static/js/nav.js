e36d9c7ca47d29bd503bacaef631c95e25d12923
timmorrisdev <timmorris_16@hotmail.com>
2022-01-20 16:02:49 +0000

    Style navbar logos

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,10 +1,14 @@
 $(document).ready(function () {
 
     $('.dropdown').on('mouseover', function () {
-        $(this).children('.dropdown-menu').css('display', 'block')
+        $(this).children('.dropdown-menu').css({
+            'display': 'block',
+        })
     })
     $('.dropdown').on('mouseout', function () {
-        $(this).children('.dropdown-menu').css('display', 'none')
+        $(this).children('.dropdown-menu').css({
+            'display': 'none',
+        })
     })
 
 })
