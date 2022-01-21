a358646713ba963ef7084ddb25620dbcf0dbce7e
timmorrisdev <timmorris_16@hotmail.com>
2022-01-21 11:37:38 +0000

    Fix issue with anonymous users leaving reviews, add update product rating logic to view

--- product_reviews/views.py
+++ product_reviews/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 # from django.contrib.auth.decorators import login_required
 
 from products.models import Product
+from .models import ProductReview
 
 from .forms import ProductReviewForm
 
@@ -16,15 +17,19 @@ def add_product_review(request, product_id):
         form = ProductReviewForm(request.POST)
         if form.is_valid():
             review = form.save(commit=False)
-            review.reviewer = request.user
+            if request.user.is_authenticated:
+                review.reviewer = request.user
             review.product = product
             review.save()
+            update_product_rating(product)
             messages.success(request, 'Successfully added product review!')
             return redirect(reverse('product_detail', args=[product_id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
         form = ProductReviewForm()
+
+    
 
     template = 'product_reviews/add_product_review.html'
     context = {
@@ -33,3 +38,20 @@ def add_product_review(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def update_product_rating(product):
+    # update product rating
+
+    reviews = ProductReview.objects.all()
+    product_reviews = reviews.filter(product=product)
+
+    total = 0
+    for i in product_reviews:
+        total += i.rating
+
+    new_rating = round(total / len(product_reviews), 2)
+    print(new_rating)
+    product.rating = new_rating
+    product.save()
+
