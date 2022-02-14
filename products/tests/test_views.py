8a23cdb267f5baa22bcb6ef69a3906548de044a3
timmorrisdev <timmorris_16@hotmail.com>
2022-02-14 18:08:07 +0000

    Add credit section to readme.md

---
+++ products/tests/test_views.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+
+# class TestViews(TestCase):
+#     '''Tests for views.py '''
+
+#     def test_get_all_products(self):
+#         '''Test getting all products and displaying'''
+
+#         response = self.client.get('products/add')
+#         self.assertEqual(response.status_code, 200)
+#         self.assertTemplateUsed(response, 'products/add_product.html')
