58061cf82bac09daa874e42b61e709119815d4f3
timmorrisdev <timmorris_16@hotmail.com>
2022-02-17 16:46:15 +0000

    Add product POST request test

--- products/tests/test_views.py
+++ products/tests/test_views.py
@@ -60,7 +60,7 @@ class TestProductViews(TestCase):
         )
 
         # print(self.product1.category)
-        
+
         # urls
         self.products_url = reverse('products')
         self.detail_url = reverse('product_detail', args=[self.product1.pk])
@@ -70,30 +70,32 @@ class TestProductViews(TestCase):
         self.add_product_variation_url = reverse('add_product_variation', args=[self.product1.pk])
         self.edit_product_variation_url = reverse('edit_product_variation', args=[self.product_variation1.pk])
         self.delete_product_variation_url = reverse('delete_product_variation', args=[self.product_variation1.pk])
-        
 
-    def test_product_list_GET(self):
+    def test_product_list_get(self):
+        '''test get all products'''
 
         # test response
         response = self.client.get(self.products_url)
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/products.html')
-    
-    def test_product_detail_GET(self):
+
+    def test_product_detail_get(self):
+        '''test product detail get request'''
 
         # test response
         response = self.client.get(self.detail_url)
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/product_detail.html')
 
-    def test_add_product_GET_not_superuser(self):
+    def test_add_product_get_not_superuser(self):
+        '''test add product get request'''
 
         # user login
         self.client.login(username="testuser", password="usertestpassword")
 
         # test response
         response = self.client.get(self.add_product_url)
-        self.assertEquals(response.status_code, 302)
+        self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('home'))
 
         # test messages
@@ -101,100 +103,146 @@ class TestProductViews(TestCase):
         self.assertEqual(len(messages), 1)
         self.assertEqual(str(messages[0]), 'Sorry, only store owners can do that.')
 
-    def test_add_product_GET_superuser(self):
+    def test_add_product_get_superuser(self):
+        '''test add product get request'''
 
         # superuser login
         self.client.login(username="testsuper", password="supertestpassword")
 
         # test response
         response = self.client.get(self.add_product_url)
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/add_product.html')
-    
-    def test_add_product_POST_form_valid(self):
+
+    def test_add_product_post_form_valid(self):
+        '''test add product post request'''
 
         # superuser login
         self.client.login(username="testsuper", password="supertestpassword")
-        # self.client.force_login(self.super_user)
 
         # test response
-        # response = self.client.post(self.add_product_url, {
-        #     'code': '5678',
-        #     'name': 'test_product2',
-        #     'description': 'this is a product2',
-        #     'has_variations': True,
-        #     'price': 12.34,
-        #     'brand': 'brand',
-        #     'category': self.category1
-        # })
+        response = self.client.post(self.add_product_url, {
+            'name': 'test_product2',
+            'description': 'this is a product2',
+            'has_variations': False,
+            'price': 12.34,
+            'brand': 'brand',
+        })
 
-        # product = Product.objects.get(name='test_product2')
-        # print(product)
-        # print(response)
+        product = Product.objects.get(name='test_product2')
+        self.assertEqual(product.name, 'test_product2')
 
-    def test_edit_product_GET_not_superuser(self):
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('product_detail', args=(product.id,)))
+
+    def test_add_product_with_variations_post_form_valid(self):
+        '''test add product post request'''
+
+        # superuser login
+        self.client.login(username="testsuper", password="supertestpassword")
+
+        # test response
+        response = self.client.post(self.add_product_url, {
+            'name': 'test_product2',
+            'description': 'this is a product2',
+            'has_variations': True,
+            'price': 12.34,
+            'brand': 'brand',
+        })
+
+        product = Product.objects.get(name='test_product2')
+        self.assertEqual(product.name, 'test_product2')
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('add_product_variation', args=(product.id,)))
+    
+    def test_add_product_post_form_invalid(self):
+        '''test add product post request form invalid'''
+
+        # superuser login
+        self.client.login(username="testsuper", password="supertestpassword")
+
+        # test response
+        response = self.client.post(self.add_product_url, {
+            'name': '',
+            'description': '',
+            'price': '',
+            'brand': 'brand',
+        })
+
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]), 'Failed to add product. \
+                                      Please ensure the form is valid.')
+
+    def test_edit_product_get_not_superuser(self):
+        '''test edit product get'''
 
         # user login
         self.client.login(username="testuser", password="usertestpassword")
 
         # test response
         response = self.client.get(self.edit_product_url)
-        self.assertEquals(response.status_code, 302)
+        self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('home'))
 
         # test messages
         messages = list(get_messages(response.wsgi_request))
         self.assertEqual(len(messages), 1)
         self.assertEqual(str(messages[0]), 'Sorry, only store owners can do that.')
-    
-    def test_edit_product_GET_superuser(self):
+
+    def test_edit_product_get_superuser(self):
+        '''test edit product get'''
 
         # superuser login
         self.client.login(username="testsuper", password="supertestpassword")
 
         # test response
         response = self.client.get(self.edit_product_url)
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/edit_product.html')
 
         # test messages
         messages = list(get_messages(response.wsgi_request))
         self.assertEqual(len(messages), 1)
         self.assertEqual(str(messages[0]), f'You are editing {self.product1.name}')
-    
-    def test_delete_product_GET_not_superuser(self):
+
+    def test_delete_product_get_not_superuser(self):
+        '''test delete product not superuser'''
 
         # user login
         self.client.login(username="testuser", password="usertestpassword")
 
         # test response
         response = self.client.get(self.delete_product_url)
-        self.assertEquals(response.status_code, 302)
+        self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('home'))
 
         # test messages
         messages = list(get_messages(response.wsgi_request))
         self.assertEqual(len(messages), 1)
         self.assertEqual(str(messages[0]), 'Sorry, only store owners can do that.')
-    
-    def test_delete_product_GET_superuser(self):
+
+    def test_delete_product_get_superuser(self):
+        '''test delete product superuser'''
 
         # superuser login
         self.client.login(username="testsuper", password="supertestpassword")
 
         # test response
         response = self.client.get(self.delete_product_url)
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/delete_product.html')
 
-    def test_add_product_variation_GET_not_superuser(self):
+    def test_add_product_variation_get_not_superuser(self):
+        '''test add product variation not superuser'''
 
         # user login
         self.client.login(username="testuser", password="usertestpassword")
 
         # test response
         response = self.client.get(self.add_product_variation_url)
-        self.assertEquals(response.status_code, 302)
+        self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('home'))
 
         # test messages
@@ -202,39 +250,42 @@ class TestProductViews(TestCase):
         self.assertEqual(len(messages), 1)
         self.assertEqual(str(messages[0]), 'Sorry, only store owners can do that.')
 
-    def test_add_product_variation_GET_superuser(self):
+    def test_add_product_variation_get_superuser(self):
+        '''test add product variation superuser'''
 
         # superuser login
         self.client.login(username="testsuper", password="supertestpassword")
 
         # test response
         response = self.client.get(self.add_product_variation_url)
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/add_product_variation.html')
-    
-    def test_edit_product_variation_GET_not_superuser(self):
+
+    def test_edit_product_variation_get_not_superuser(self):
+        '''test edit product variation not superuser'''
 
         # user login
         self.client.login(username="testuser", password="usertestpassword")
 
         # test response
         response = self.client.get(self.edit_product_variation_url)
-        self.assertEquals(response.status_code, 302)
+        self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('home'))
 
         # test messages
         messages = list(get_messages(response.wsgi_request))
         self.assertEqual(len(messages), 1)
         self.assertEqual(str(messages[0]), 'Sorry, only store owners can do that.')
-    
-    def test_edit_product_variation_GET_superuser(self):
+
+    def test_edit_product_variation_get_superuser(self):
+        '''test edit product variation superuser'''
 
         # superuser login
         self.client.login(username="testsuper", password="supertestpassword")
 
         # test response
         response = self.client.get(self.edit_product_variation_url)
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/edit_product_variation.html')
 
         # test messages
@@ -244,14 +295,15 @@ class TestProductViews(TestCase):
                                     {self.product_variation1.name} on \
                                     {self.product_variation1.product.name}')
         
-    def test_delete_product_variation_GET_not_superuser(self):
+    def test_delete_product_variation_get_not_superuser(self):
+        '''test delete product variation not superuser'''
 
         # user login
         self.client.login(username="testuser", password="usertestpassword")
 
         # test response
         response = self.client.get(self.delete_product_variation_url)
-        self.assertEquals(response.status_code, 302)
+        self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('home'))
 
         # test messages
@@ -259,14 +311,15 @@ class TestProductViews(TestCase):
         self.assertEqual(len(messages), 1)
         self.assertEqual(str(messages[0]), 'Sorry, only store owners can do that.')
     
-    def test_delete_product_variation_GET_superuser(self):
+    def test_delete_product_variation_get_superuser(self):
+        '''test delete product variation superuser'''
 
         # superuser login
         self.client.login(username="testsuper", password="supertestpassword")
 
         # test response
         response = self.client.get(self.delete_product_variation_url)
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/delete_product_variation.html')
 
 
