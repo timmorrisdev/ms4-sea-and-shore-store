437406c9caff1c3ca2419b6337ef6aa6af5ab605
timmorrisdev <timmorris_16@hotmail.com>
2022-02-21 17:52:56 +0000

    Add 'overflow scroll' to brands dropdown menu

--- sea_and_shore/settings.py
+++ sea_and_shore/settings.py
@@ -26,7 +26,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('MS4_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['sea-and-shore-store.herokuapp.com', 'localhost']
